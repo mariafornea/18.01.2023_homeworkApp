3c32197ee05bf2d4e4a3e941e218afd46adcd005
mariafornea <forneamaria2022@gmail.com>
2023-03-03 21:34:19 +0100

    added users

---
+++ app/src/Components/UserContainer/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Users from '../Users'
+export default function UserContainer() {
+  const Users = [
+    {id: 1, firstname: 'Anthony', lastname: 'Mackie', age: 44},
+    {id: 2, firstname: 'Helena Bonham', lastname: 'Carter', age: 56},
+    {id: 3, firstname: 'Millie Bobby', lastname: 'Brown', age: 19},
+    {id: 4, firstname: 'Chris', lastname: 'Hemsworth', age: 39},
+    {id: 5, firstname: 'Elliot', lastname: 'Page', age: 36},
+    {id: 6, firstname: 'Pedro', lastname: 'Pascal', age: 47},
+    {id: 7, firstname: 'Adam', lastname: 'Sandler', age: 56},
+    {id: 8, firstname: 'Benedict', lastname: 'Wong', age: 51},
+    {id: 9, firstname: 'Kurt', lastname: 'Russell', age: 71},
+    {id: 10, firstname: 'Meryl', lastname: 'Streep', age: 73}
+  ]
+  return (
+    <div>UserContainer</div>
+  )
+}
